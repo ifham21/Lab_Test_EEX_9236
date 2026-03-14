0d21334493ab3ff2631fcba0581d50aae3976989
Ahmed Ifham <66723461+ifham21@users.noreply.github.com>
2026-03-14 15:19:23 +0530

    Problem Statement and Read the selected data from the data set

--- 626102021_lab_test.py
+++ 626102021_lab_test.py
@@ -1,3 +1,15 @@
 # 1. Define a problem statement
-# Identify and clearly explain a real-world problem that can be addressed using the two
-# selected datasets.
+'''
+This project analyzes two Facebook network datasets (1684.edges and 1912.edges) to identify user connectivity patterns
+and determine the most connected users in each network.
+'''
+import pandas as pd
+
+
+# 2. Read the datasets into Python
+data1 = pd.read_csv("1684.edges", sep=" ", header=None) # Load dataset 1
+data2 = pd.read_csv("1912.edges", sep=" ", header=None) # Load dataset 2
+print("Dataset 1 sample:") # printed the selected output of dataset 01
+print(data1.head())
+print("\nDataset 2 sample:") # printed the selected output of dataset 02
+print(data2.head())
