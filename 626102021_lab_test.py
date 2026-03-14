efe100889e642008de02bbab8b5ae2839b127dcb
Ahmed Ifham <66723461+ifham21@users.noreply.github.com>
2026-03-14 15:23:51 +0530

    Question 3
    Import numpy and then using the numpy I have printed those Datasets' statistics

--- 626102021_lab_test.py
+++ 626102021_lab_test.py
@@ -4,6 +4,7 @@ This project analyzes two Facebook network datasets (1684.edges and 1912.edges) 
 and determine the most connected users in each network.
 '''
 import pandas as pd
+import numpy as np
 
 
 # 2. Read the datasets into Python
@@ -13,3 +14,19 @@ print("Dataset 1 sample:") # printed the selected output of dataset 01
 print(data1.head())
 print("\nDataset 2 sample:") # printed the selected output of dataset 02
 print(data2.head())
+
+# 3. Compute summary statistics
+print("\nDataset 1 Statistics")
+print("Mean:", np.mean(data1[0]))
+print("Minimum value:", np.min(data1[0]))
+print("Maximum value:", np.max(data1[0]))
+print("Standard deviation:", np.std(data1[0]))
+
+print("\nDataset 2 Statistics")
+print("Mean:", np.mean(data2[0]))
+print("Minimum value:", np.min(data2[0]))
+print("Maximum value:", np.max(data2[0]))
+print("Standard deviation:", np.std(data2[0]))
+
+
+# 4. Design and implement an algorithm
